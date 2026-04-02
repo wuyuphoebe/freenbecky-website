c0322441953b79175adb0a1b32bfbf800161f693
wuyuphoebe <wuyuphoebe@gmail.com>
2026-04-02 20:04:39 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+const FB_DATA = [
+    {
+        id: 1,
+        name: "Siam Paragon",
+        celebrity: "Freen & Becky",
+        date: "2024-03-20",
+        lat: 13.7468,
+        lng: 100.5350,
+        img: "images/photo1.jpg", 
+        // 這裡填入你想要導向的 Google Maps 專屬連結
+        mapUrl: "https://maps.app.goo.gl/gKw6zQQWjiCYAUEi9",
+        desc: "兩人的初次活動地，充滿回憶的起點。"
+    },
+    {
+        id: 2,
+        name: "Central World",
+        celebrity: "Freen",
+        date: "2024-01-15",
+        lat: 13.7462,
+        lng: 100.5399,
+        img: "images/photo2.jpg",
+        mapUrl: "https://maps.app.goo.gl/xxxxxxxxx",
+        desc: "雜誌拍攝現場。"
+    }
+];
